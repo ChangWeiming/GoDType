f9f33551a21497ea9fe8c1fa1ad1414e7d37b880
MuZhou233 <muzhou233@outlook.com>
2020-04-09 00:46:13 +0900

    move files

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,22 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const path = require('path')
 
-module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: 'css',
-  }),
-);
+module.exports = {
+  paths: function (paths, env) {
+      paths.appIndexJs = path.resolve(__dirname, "appsrc/index.tsx");
+      paths.appSrc = path.resolve(__dirname, "appsrc");
+      paths.testsSetup = path.resolve(__dirname, "appsrc/setupTests");
+      paths.proxySetup = path.resolve(__dirname, "appsrc/setupProxy.js");
+      paths.appTypeDeclarations = path.resolve(__dirname, "appsrc/react-app-env.d.ts");
+
+      return paths;
+  },
+  webpack: override(
+      fixBabelImports('import', {
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+          style: 'css',
+      }),
+      addLessLoader(),
+  )
+}
